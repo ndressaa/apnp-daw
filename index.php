f4e861e68cb738c85569c387dab996899c6ad807
Andressa <andressajucoski@gmail.com>
2021-11-21 16:43:38 -0200

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Atividade 1</title>
+  </head>
+  <body>
+    <div>
+      <form action="">
+        <input type="text" placeholder="Pesquisar Produtos">
+        <button>&#x1F50E;</button>
+      </form>
+    </div>
+  </body>
+</html>
